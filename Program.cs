5216bbde997444bee038a678af8f84c947d9e943
BuildTools <japa4551@protonmail.com>
2021-03-27 12:55:43 -0300

    Added the actual code (probably ready) and updated the README.md once again

---
+++ Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace JapaBookmarksDateViewer
+{
+    class Program
+    {
+        static string name = "Japa's Bookmark Date Viewer";
+        static int current_version = 1;
+        static string window_title = $"{Program.name} v{current_version} by japa4551";
+
+        static void Main(string[] input_file_array)
+        {
+            Console.Title = $"{window_title} - Awaiting User...";
+            // file_array can either be from Terminal/Console or Drag-and-Drop operation
+
+            string executable = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string executable_directory = System.IO.Path.GetDirectoryName(executable);
+
+            string[] exec_folder_html_file_array = Directory.GetFiles(executable_directory, "*.html", SearchOption.TopDirectoryOnly);
+
+            List<string> html_file_list = new List<string>();
+
+            foreach(string input_html_file_path in input_file_array)
+            {
+                //Console.WriteLine(html_file);
+
+                html_file_list.Add(input_html_file_path);
+            }
+
+            foreach(string exec_folder_html_file_path in exec_folder_html_file_array)
+            {
+                //Console.WriteLine(html_file);
+
+                html_file_list.Add(exec_folder_html_file_path);
+            }
+
+            if(html_file_list.Count == 1)
+            {
+                Console.Write($"The program received 1 Input File from the ");
+                if(input_file_array.Length == 1) Console.WriteLine("User");
+                if(exec_folder_html_file_array.Length == 1) Console.WriteLine("Executable Folder");
+            }
+
+            else if(html_file_list.Count > 1)
+            {
+                Console.WriteLine($"The program found a total of {input_file_array.Length + exec_folder_html_file_array.Length} " +
+                "HTML files: \n");
+
+                if(input_file_array.Length >= 1) Console.WriteLine($"- {input_file_array.Length} file is from Direct Input");
+                else if(input_file_array.Length > 1) Console.WriteLine($"- {input_file_array.Length} files are from Direct Input");
+
+                if(exec_folder_html_file_array.Length >= 1) Console.WriteLine($"- {exec_folder_html_file_array.Length} file is from the executable folder");
+                else if(exec_folder_html_file_array.Length > 1) Console.WriteLine($"- {exec_folder_html_file_array.Length} files are from the executable folder");
+            }
+
+            else
+            {
+                Console.WriteLine("Input File is empty!");
+
+                Halt_Program();
+            }
+
+            Console.WriteLine("\nPress ENTER to generate the output file");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.Title = $"{window_title} - Reading HTML files...";
+
+            foreach(string html_file_path in html_file_list)
+            {
+                //Console.WriteLine(html_file_path);
+
+                Read_HTML(html_file_path);
+            }
+
+            Halt_Program();
+        }
+
+        static DateTime Convert_UNIX_To_UTC(int input)
+        {
+            DateTime date_time = new DateTime(1970,1,1,0,0,0,0,System.DateTimeKind.Utc);
+            date_time = date_time.AddSeconds(input).ToLocalTime();
+
+            return date_time;
+        }
+
+        static void Halt_Program()
+        {
+            Console.Title = $"{window_title} - End.";
+
+            Console.WriteLine("\nPress Enter to close the program.");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+
+        static void Read_HTML(string input_file_path)
+        {
+            string input_file_name = Path.GetFileName(input_file_path);
+
+            string exe_path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string exe_directory = System.IO.Path.GetDirectoryName(exe_path);
+           
+            var document = new HtmlDocument();
+
+            try
+            {
+                document.Load(input_file_path);
+
+                var bookmark_nodes = document.DocumentNode.SelectNodes("//a");
+                var bookmark_folders_node = document.DocumentNode.SelectNodes("//h3");
+
+                List<string> lines = new List<string>();
+
+                foreach(var node in bookmark_nodes)
+                {
+                    lines.Add($"Name: {WebUtility.HtmlDecode(node.InnerHtml)}");
+                    lines.Add($"Link: {node.GetAttributeValue<string>("href", null)}");
+                    lines.Add($"Date Added: {Convert_UNIX_To_UTC(node.GetAttributeValue<int>("ADD_DATE", 0))}\n");
+                }
+                
+                File.WriteAllLines($"{exe_directory}\\{input_file_name}_output.txt", lines);
+
+                Console.WriteLine($"Success! Saved output as {input_file_name}_output.txt");
+            }
+
+            catch
+            {
+                Console.WriteLine($"{input_file_name} IS AN INVALID HTML FILE! (Maybe its corrupt?)");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,27 +1,41 @@
 # JapaBookmarksDateViewer
 
 ## Requieres .NET Core 5.0
+## Program uses HtmlAgilityPack v 1.11.32 by zzzprojects
 
  A simple C# tool built to View the Date of an Bookmarks Export.
 
- This program was originally on Japa's Unified Test Program v4 (My Test Program as you noticed)
+ This program was originally on Japa's Unified Test Program v4 (My Test Toolkit)
 
  Then i decided to rewrite it since the code was pretty garbage, then post it here (Open-Source)
 
 ## Why you should use this program
-- This program will be used (even if once) if you are trying to figure out precisely when did you bookmark something
+- This program should be used (even if only once) if you are trying to figure out precisely when something was added to your bookmark
 
-- Chromium Browsers (like Chrome and Brave; so far i've only tested in Chromium Browsers) save the Bookmark's date as a [UNIX Time](https://en.wikipedia.org/wiki/Unix_time) but for some reason the End-User wont be able to see it normally, if he does not have the knowledge to do so.
+- Chromium Browsers (like Chrome and Brave; so far i've only tested in Chromium Browsers) save the Bookmark's date as a [UNIX Time](https://en.wikipedia.org/wiki/Unix_time) but for some reason the End-User cant access it normally if he doesnt have the knowledge to do so.
 
-- This program as previously stated, will help you to find this pesky little [UNIX Time](https://en.wikipedia.org/wiki/Unix_time) and convert it to actual Date that you can read
+- This program as previously stated, will help you to find this pesky little [UNIX Time](https://en.wikipedia.org/wiki/Unix_time) and convert it to [UTC](https://en.wikipedia.org/wiki/Coordinated_Universal_Time).
 
-- It is also Open-Source, so you can be 100% sure i wont be seeing your Homework folder ;)
+- It is also Open-Source, so you can be sure i wont be seeing whats in your Homework folder ;)
 
 ## How to use
-This program is easy to use as 1, 2, 3
+Its easy as 1, 2, 3. Just follow the instructions below
 
-- If in doubt of how you get export your Bookmarks as HTML, check [this page](https://www.wikihow.com/Export-Bookmarks-from-Chrome)
+In case you dont know how to export your Bookmarks as HTML, check [this page](https://www.wikihow.com/Export-Bookmarks-from-Chrome)
 
-- Drag and drop the Exported HTML in the Program's executable
+- Download the pre-built program on the [Releases page](https://github.com/japa4551/JapaBookmarksDateViewer/releases) (You can also build it yourself if you want to or if there is no version for your current system)
 
-- The program will attempt to read the whole file, and then it will output what was read as output.txt
+- Drag and drop the Exported HTML in the Program's executable (or just leave a HTML file in the same place as the Executable) then launch the program
+
+- The program will attempt to read the file, if it succeeds it will save the output on the executable's path as "input_file_name_output.txt" (Example: favoritos_26_03_2021_output.txt)
+
+- You can also use multiple HTML files by placing them in the Program's Folder or by running the program on Terminal with multiple parameters (Program.exe file1.html file2.html)
+
+## Random facts
+- Previously before release, you had to manually enter the Input file's name (with extension) and path to make it work
+
+- If any data on the Input was incorrect, the program would crash
+
+- The program also was one file at the time before the rework
+
+- I don't know how to make GitHub pages properly, so it will contain literally everything of this project (including Debug Builds)
